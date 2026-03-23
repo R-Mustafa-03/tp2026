8f9d6ec806ec4e9cf33bec17d704294b2b41a15b
stepches <stepches2@gmail.com>
2026-03-23 16:38:42 +0300

    Add Shape interface for T4

--- burlachenko.stepan/T4/shape.h
+++ burlachenko.stepan/T4/shape.h
@@ -7,11 +7,13 @@
 class Shape
 {
 public:
+    virtual ~Shape() = default;
+
     virtual double getArea() const = 0;
     virtual Point getCenter() const = 0;
     virtual void move(const Point& newCenter) = 0;
     virtual void scale(double ratio) = 0;
-    virtual std::string getName() const = 0;
+    virtual std::string getName() const = 0;    
 };
 
 #endif
